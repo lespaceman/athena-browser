3a8ce6066681608cc9cce217e4dc58e96ad8f7fa
Nadeem M <n.adeem@outlook.com>
2025-10-15 03:44:30 +0530

    feat: Remove deprecated build files and configurations for browser_native module

--- athena-agent/build/Release/.deps/Release/browser_native.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/browser_native.node := ln -f "Release/obj.target/browser_native.node" "Release/browser_native.node" 2>/dev/null || (rm -rf "Release/browser_native.node" && cp -af "Release/obj.target/browser_native.node" "Release/browser_native.node")
